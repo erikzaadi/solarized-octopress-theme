dddf07e349c681d2f7c1eac3e83b30623d74bce7
Lee Green <lee.x.green@gmail.com>
2013-11-10 13:18:21 -0500

    fix github.js -- seJSONP was not actually supplying a callback name

--- source/javascripts/github.js
+++ source/javascripts/github.js
@@ -13,15 +13,15 @@ var github = (function(){
   return {
     showRepos: function(options){
       $.ajax({
-          url: "https://api.github.com/users/"+options.user+"/repos?sort=pushed&callback=?"
-        , dataType: 'jsonp'
+          url: "https://api.github.com/users/"+options.user+"/repos?sort=pushed"
+        , dataType: 'json'
         , error: function (err) { $(options.target + ' li.loading').addClass('error').text("Error loading feed"); }
         , success: function(data) {
           var repos = [];
-          if (!data || !data.data) { return; }
-          for (var i = 0; i < data.data.length; i++) {
-            if (options.skip_forks && data.data[i].fork) { continue; }
-            repos.push(data.data[i]);
+          if (!data ) { return; }
+          for (var i = 0; i < data.length; i++) {
+            if (options.skip_forks && data[i].fork) { continue; }
+            repos.push(data[i]);
           }
           if (options.count) { repos.splice(options.count); }
           render(options.target, repos);
